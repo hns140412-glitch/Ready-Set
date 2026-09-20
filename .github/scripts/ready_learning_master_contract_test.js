93877b410c1ba09c8a059caa43ca7457254b46eb
hns140412-glitch <hns140412@gmail.com>
2026-09-20 18:30:38 +0900

    test: align unit-map version lock with hardened v0.2.1

--- .github/scripts/ready_learning_master_contract_test.js
+++ .github/scripts/ready_learning_master_contract_test.js
@@ -295,7 +295,7 @@ const noUnitContext=standardMatcher.match('과학',{teacher_instruction:'지층�
 assert.strictEqual(noUnitContext.official_standard_code,'6과01-01');
 assert.strictEqual(noUnitContext.unit_mapping_evidence.status,'UNIT_MAPPING_EVIDENCE_AVAILABLE_NOT_APPLIED');
 
-assert.strictEqual(unitMap.version,'0.2.0');
+assert.strictEqual(unitMap.version,'0.2.1');
 assert.strictEqual(unitMap.RECORDS.length,196);
 for(const [subject,expected] of Object.entries({국어:34,사회:27,수학:45,과학:51})){
   const cov=unitMap.coverage(subject);
